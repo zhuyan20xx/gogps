8874792e15d5c603d77837fc8ffa33100bc5ea78
Eugenio <Eugenio@localhost>
2010-11-17 19:40:59 +0900

    Changed copyright information on org.gogpsproject files

--- src/main/java/org/gogpsproject/Constants.java
+++ src/main/java/org/gogpsproject/Constants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, Eugenio Realini. All Rights Reserved.
+ * Copyright (c) 2010, Eugenio Realini, Mirko Reguzzoni. All Rights Reserved.
  *
  * This file is part of goGPS Project (goGPS).
  *
