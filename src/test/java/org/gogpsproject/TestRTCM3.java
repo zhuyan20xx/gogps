3373526d8f9c18333c75a672d0e704af37a08b67
MrPatol <MrPatol@gmail.com>
2011-09-29 08:49:26 +0200

    added new test

---
+++ src/test/java/org/gogpsproject/TestRTCM3.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2010, Cryms.com . All Rights Reserved.
+ *
+ * This file is part of goGPS Project (goGPS).
+ *
+ * goGPS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * goGPS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with goGPS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.gogpsproject;
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.PortInUseException;
+
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Vector;
+
+import org.gogpsproject.ObservationsBuffer;
+import org.gogpsproject.parser.rtcm3.RTCM3Client;
+import org.gogpsproject.parser.ublox.UBXSerialConnection;
+
+@SuppressWarnings("restriction")
+public class TestRTCM3 {
+
+	/**
+	 * @param args
+	 */
+
+	
+
+	public static void main(String[] args) {
+		
+
+
+		try {
+			RTCM3Client rtcm = RTCM3Client.getInstance("www.euref-ip.net", 80, args[0],args[1], "ZIM20");
+			//RTCM3Client rtcm = RTCM3Client.getInstance("ntrip.jenoba.jp", 80, args[0],args[1], "JVR30");
+			rtcm.setDebug(true);
+			// Ntrip-GAA: $GPGGA,183836,3435.524,N,13530.231,E,4,10,1,164,M,1,M,3,0*69
+			// CH Manno
+			Coordinates coordinates = Coordinates.globalXYZInstance(4382366.510741806,687718.046802147,4568060.791344867);
+			// JP Osaka
+			//Coordinates coordinates = Coordinates.globalXYZInstance(-3749314.940644724,3684015.867703885,3600798.5084946174);
+			rtcm.setMasterPosition(coordinates);
+			rtcm.init();
+
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
+//		System.out.println(computeNMEACheckSum("$GPGGA,200530,4600,N,00857,E,4,10,1,200,M,1,M,3,0"));
+//
+//		File f = new File("./data/rtcm.out");
+//		try {
+//			FileInputStream fis = new FileInputStream(f);
+//			RTCM3Client cl = new RTCM3Client(null);
+//			cl.go = true;
+//			cl.debug = true;
+//			cl.readLoop(fis);
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+
+	
+	}
+
+}
