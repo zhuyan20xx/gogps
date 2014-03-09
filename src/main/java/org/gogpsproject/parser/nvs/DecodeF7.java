029b80d7a573ab77f3d5c466b01db9423e988955
nro2dai <nro2dai@localhost>
2014-03-09 21:44:06 +0900

    working on F7h decoder.

--- src/main/java/org/gogpsproject/parser/nvs/DecodeF7.java
+++ src/main/java/org/gogpsproject/parser/nvs/DecodeF7.java
@@ -62,189 +62,344 @@ public class DecodeF7 {
 		int satType = in.read();  // satType 1 = GPS, 2 = GLONASS		
 		System.out.println("satType: " + satType);  
 		int satId = in.read();
-		System.out.println("svid: " + satId);  
 		
-		eph.setSatID((int)satId);
-
-		byte bytes[];
-		int signInt;
-		String signStr; 
-		int espInt;
-		String espStr;
-		long mantInt;
-		String mantStr; 
-		double mantInt2;
-		double crs;
 		
+		if (satType == 1){ 
+		
+				System.out.println("GPS PRN: " + satId);  
 			
-		/*  Crs, 4 bytes  */		
-		bytes = new byte[4];
-		in.read(bytes, 0, bytes.length);
-		long longCrs = Bits.byteToLong(bytes);
-		//System.out.println("longCrs: "+ longCrs);
-		String binCrs = Long.toBinaryString(longCrs);
-		binCrs = String.format("%32s",binCrs).replace(' ', '0');
-		//System.out.println("binCrs: "+ binCrs);
+				eph.setSatID((int)satId);
+		
+				byte bytes[];
+				int signInt;
+				String signStr; 
+				int espInt;
+				String espStr;
+				long mantInt;
+				String mantStr; 
+				double mantInt2;
+				double crs;
+			
+			
+				/*  Crs, 4 bytes  */		
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longCrs = Bits.byteToLong(bytes);
+				//System.out.println("longCrs: "+ longCrs);
+				String binCrs = Long.toBinaryString(longCrs);
+				binCrs = String.format("%32s",binCrs).replace(' ', '0');
+				//System.out.println("binCrs: "+ binCrs);
+						
+				signStr = binCrs.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binCrs.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binCrs.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = (double) (mantInt / Math.pow(2, 23));
+		        crs = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Crs: "+ crs);
+			
+		        
+		        /*  Dn, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longDn = Bits.byteToLong(bytes);
+				String binDn = Long.toBinaryString(longDn);
+				binDn = String.format("%32s",binDn).replace(' ', '0');
 				
-		signStr = binCrs.substring(0,1);
-        signInt = Integer.parseInt(signStr, 2);
-        espStr = binCrs.substring(1,9);
-        espInt = Integer.parseInt(espStr, 2);
-        mantStr = binCrs.substring(9);
-        mantInt = Integer.parseInt(mantStr, 2);
-        mantInt2 = (double) (mantInt / Math.pow(2, 23));
-        crs = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
-        System.out.println("Crs: "+ crs);
-	
-        
-        /*  Dn, 4 bytes  */
-		bytes = new byte[4];
-		in.read(bytes, 0, bytes.length);
-		long longDn = Bits.byteToLong(bytes);
-		String binDn = Long.toBinaryString(longDn);
-		binDn = String.format("%32s",binDn).replace(' ', '0');
+				signStr = binDn.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binDn.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binDn.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double dn = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Dn: "+ dn);
+		   
+		        
+		        /*  M0, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);
+				double m0 = Bits.byteToIEEE754Double(bytes);
+				System.out.println("M0: "+ m0);		
+		        
+		        /*  Cuc, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longCuc = Bits.byteToLong(bytes);
+				String binCuc = Long.toBinaryString(longCuc);
+				binCuc = String.format("%32s",binCuc).replace(' ', '0');
+				
+				signStr = binCuc.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binCuc.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binCuc.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double cuc = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Cuc: "+ cuc);
+		        
+		        
+		        /*  E, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);	
+				double e = Bits.byteToIEEE754Double(bytes);
+				System.out.println("E: "+ e);		
+		        
+		        /*  Cus, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longCus = Bits.byteToLong(bytes);
+				String binCus = Long.toBinaryString(longCus);
+				binCus = String.format("%32s",binCus).replace(' ', '0');
+				
+				signStr = binCus.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binCus.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binCus.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double cus = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Cus: "+ cus);
+		        
+		        
+		        /*  SqrtA, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);
+				double sqrtA = Bits.byteToIEEE754Double(bytes);
+				System.out.println("SqrtA: "+ sqrtA);
+			
+		        
+		        /*  Toe, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);
+				double toe = Bits.byteToIEEE754Double(bytes);
+				System.out.println("Toe: "+ toe);
+				  
+		        
+		        /*  Cic, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longCic = Bits.byteToLong(bytes);
+				String binCic = Long.toBinaryString(longCic);
+				binCic = String.format("%32s",binCic).replace(' ', '0');
+				
+				signStr = binCic.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binCic.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binCic.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double cic = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Cic: "+ cic);
+		        
+		        
+		        /*  Omega0, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);
+				double omega0 = Bits.byteToIEEE754Double(bytes);
+				System.out.println("Omega0: "+ omega0);
+		        
+				
+				/*  Cis, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longCis = Bits.byteToLong(bytes);
+				String binCis = Long.toBinaryString(longCis);
+				binCis = String.format("%32s",binCis).replace(' ', '0');
+				
+				signStr = binCis.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binCis.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binCis.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double cis = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Cis: "+ cis);
 		
-		signStr = binDn.substring(0,1);
-        signInt = Integer.parseInt(signStr, 2);
-        espStr = binDn.substring(1,9);
-        espInt = Integer.parseInt(espStr, 2);
-        mantStr = binDn.substring(9);
-        mantInt = Integer.parseInt(mantStr, 2);
-        mantInt2 = mantInt / Math.pow(2, 23);
-        double dn = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
-        System.out.println("Dn: "+ dn);
-   
-        
-        /*  M0, 8 bytes  */
-		bytes = new byte[8];
-		in.read(bytes, 0, bytes.length);
-		double m0 = Bits.byteToIEEE754Double(bytes);
-		System.out.println("M0: "+ m0);		
-        
-        /*  Cuc, 4 bytes  */
-		bytes = new byte[4];
-		in.read(bytes, 0, bytes.length);
-		long longCuc = Bits.byteToLong(bytes);
-		String binCuc = Long.toBinaryString(longCuc);
-		binCuc = String.format("%32s",binCuc).replace(' ', '0');
+		        
+		        /*  I0, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);
+				double i0 = Bits.byteToIEEE754Double(bytes);
+				System.out.println("I0: "+ i0);
 		
-		signStr = binCuc.substring(0,1);
-        signInt = Integer.parseInt(signStr, 2);
-        espStr = binCuc.substring(1,9);
-        espInt = Integer.parseInt(espStr, 2);
-        mantStr = binCuc.substring(9);
-        mantInt = Integer.parseInt(mantStr, 2);
-        mantInt2 = mantInt / Math.pow(2, 23);
-        double cuc = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
-        System.out.println("Cuc: "+ cuc);
+				
+				/*  Crc, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longCrc = Bits.byteToLong(bytes);
+				String binCrc = Long.toBinaryString(longCrc);
+				binCrc = String.format("%32s",binCrc).replace(' ', '0');
+				
+				signStr = binCrc.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binCrc.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binCrc.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double crc = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Crc: "+ crc);
+		        
+		        
+		        /*  W, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);
+				double w = Bits.byteToIEEE754Double(bytes);
+				System.out.println("W: "+ w);
+				
+				
+				/*  OmegaR, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);
+				double omegaR = Bits.byteToIEEE754Double(bytes);
+				System.out.println("OmegaR: "+ omegaR);
+		        
+				
+				/*  IDOT, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);
+				double idot = Bits.byteToIEEE754Double(bytes);
+				System.out.println("IDOT: "+ idot);
+				
+				
+				/*  Tgd, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longTgd = Bits.byteToLong(bytes);
+				String binTgd = Long.toBinaryString(longTgd);
+				binTgd = String.format("%32s",binTgd).replace(' ', '0');
+				
+				signStr = binTgd.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binTgd.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binTgd.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double tgd = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Tgd: "+ tgd);
+		        
+		        
+		        /*  Toc, 8 bytes  */
+				bytes = new byte[8];
+				in.read(bytes, 0, bytes.length);
+				double toc = Bits.byteToIEEE754Double(bytes);
+				System.out.println("toc: "+ toc);
+		        
+				
+				/*  Af2, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longAf2 = Bits.byteToLong(bytes);
+				String binAf2 = Long.toBinaryString(longAf2);
+				binAf2 = String.format("%32s",binAf2).replace(' ', '0');
+				
+				signStr = binAf2.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binAf2.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binAf2.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double af2 = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Af2: "+ af2);
+				
+		        
+		        /*  Af1, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longAf1 = Bits.byteToLong(bytes);
+				String binAf1 = Long.toBinaryString(longAf1);
+				binAf2 = String.format("%32s",binAf1).replace(' ', '0');
+				
+				signStr = binAf1.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binAf1.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binAf1.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double af1 = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Af1: "+ af1);
+		        
+		        
+		        /*  Af0, 4 bytes  */
+				bytes = new byte[4];
+				in.read(bytes, 0, bytes.length);
+				long longAf0 = Bits.byteToLong(bytes);
+				String binAf0 = Long.toBinaryString(longAf0);
+				binAf0 = String.format("%32s",binAf0).replace(' ', '0');
+				
+				signStr = binAf0.substring(0,1);
+		        signInt = Integer.parseInt(signStr, 2);
+		        espStr = binAf0.substring(1,9);
+		        espInt = Integer.parseInt(espStr, 2);
+		        mantStr = binAf0.substring(9);
+		        mantInt = Integer.parseInt(mantStr, 2);
+		        mantInt2 = mantInt / Math.pow(2, 23);
+		        double af0 = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
+		        System.out.println("Af0: "+ af0);
         
+		        
+		        /*  URA(svaccur), 2 bytes  */
+				bytes = new byte[2];
+				in.read(bytes, 0, bytes.length);
+				long longURA = Bits.byteToLong(bytes);
+				System.out.println("URA: " + longURA);
+				
+				
+				/*  IODE, 2 bytes  */
+				bytes = new byte[2];
+				in.read(bytes, 0, bytes.length);
+				long longIODE = Bits.byteToLong(bytes);
+				System.out.println("IODE: " + longIODE);
+				
+				
+				/*  IODC, 2 bytes  */
+				bytes = new byte[2];
+				in.read(bytes, 0, bytes.length);
+				long longIODC = Bits.byteToLong(bytes);
+				System.out.println("IODC: " + longIODC);
+				
+				
+				/*  CodeL2, 2 bytes  */
+				bytes = new byte[2];
+				in.read(bytes, 0, bytes.length);
+				long longCodeL2 = Bits.byteToLong(bytes);
+				System.out.println("CodeL2: " + longCodeL2);
+				
+				
+				/*  L2_Pdata_flag, 2 bytes  */
+				bytes = new byte[2];
+				in.read(bytes, 0, bytes.length);
+				long longL2flag = Bits.byteToLong(bytes);
+				System.out.println("L2_Pdata_flag: " + longL2flag);
+				
+				
+				/*  WeekN, 2 bytes  */
+				bytes = new byte[2];
+				in.read(bytes, 0, bytes.length);
+				long longWeekN = Bits.byteToLong(bytes);
+				System.out.println("WeekN: " + longWeekN);
+				System.out.println("+-----------------------------------+");
+						        
         
-        /*  E, 8 bytes  */
-		bytes = new byte[8];
-		in.read(bytes, 0, bytes.length);	
-		double e = Bits.byteToIEEE754Double(bytes);
-		System.out.println("E: "+ e);		
-        
-        /*  Cus, 4 bytes  */
-		bytes = new byte[4];
-		in.read(bytes, 0, bytes.length);
-		long longCus = Bits.byteToLong(bytes);
-		String binCus = Long.toBinaryString(longCus);
-		binCus = String.format("%32s",binCus).replace(' ', '0');
-		
-		signStr = binCus.substring(0,1);
-        signInt = Integer.parseInt(signStr, 2);
-        espStr = binCus.substring(1,9);
-        espInt = Integer.parseInt(espStr, 2);
-        mantStr = binCus.substring(9);
-        mantInt = Integer.parseInt(mantStr, 2);
-        mantInt2 = mantInt / Math.pow(2, 23);
-        double cus = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
-        System.out.println("Cus: "+ cus);
-        
-        
-        /*  SqrtA, 8 bytes  */
-		bytes = new byte[8];
-		in.read(bytes, 0, bytes.length);
-		double sqrtA = Bits.byteToIEEE754Double(bytes);
-		System.out.println("SqrtA: "+ sqrtA);
-	
-        
-        /*  Toe, 8 bytes  */
-		bytes = new byte[8];
-		in.read(bytes, 0, bytes.length);
-		double toe = Bits.byteToIEEE754Double(bytes);
-		System.out.println("Toe: "+ toe);
-		  
-        
-        /*  Cic, 4 bytes  */
-		bytes = new byte[4];
-		in.read(bytes, 0, bytes.length);
-		long longCic = Bits.byteToLong(bytes);
-		String binCic = Long.toBinaryString(longCic);
-		binCic = String.format("%32s",binCic).replace(' ', '0');
-		
-		signStr = binCic.substring(0,1);
-        signInt = Integer.parseInt(signStr, 2);
-        espStr = binCic.substring(1,9);
-        espInt = Integer.parseInt(espStr, 2);
-        mantStr = binCic.substring(9);
-        mantInt = Integer.parseInt(mantStr, 2);
-        mantInt2 = mantInt / Math.pow(2, 23);
-        double cic = (double) (Math.pow(-1, signInt) * Math.pow(2, (espInt - 127)) * (1 + mantInt2));  //FP32
-        System.out.println("Cic: "+ cic);
-        
-        
-        
-        
-        
-		/*
-		byte bits[] = new byte[4];
-		in.read(bits, 0, bits.length);
-		double crs = Bits.byteToIEEE754Double(bits);
-		System.out.println("CRS: " + crs );
-		*/
-		
-//		long svid=0;
-//		for(int i=3;i>=0;i--){
-//			svid=svid<<8;
-//			svid = svid | Bits.getUInt(bytes[i]);
-//
-//		}
+		}else{ 
 
-		//byte[] bytes = new byte[1];
-		//in.read(bytes, 0, bytes.length);
-	//	int[] data;
-		
-	/*	
-		int crs = in.read();
+				System.out.println("GLONASS PRN: " + satId);
 
-
-		boolean[] bits = new boolean[8];
-		int indice = 0;
-		boolean[] temp1 = Bits.intToBits(crs, 8);
-
-	
-		for (int i = 0; i < 8; i++) {
-			bits[indice] = temp1[i];
-			indice++;
 		}
-		int numSV = (int)Bits.bitsTwoComplement(bits);
-		System.out.println("NumSV :  " + numSV + " S ");
-		*/
-//		System.out.println("crs: " + crs);  
-		//float crs_bit = Bits.intToBits(crs, 8);
-		//System.out.print("crs "+ crs_bit);
-//		String binary = Integer.toBinaryString( crs );
-//		System.out.println( binary );
-		
-		
-//		System.out.print("svid "+svid);
-//		baos.write(bytes);
+        
 
-//		System.out.println("IODC("+eph.getIodc()+"), IODE2("+IODE2+"), IODE3("+IODE3+") not matching for SVID "+svid);
-
-		return null;
+			return null;
 	}
 
 
