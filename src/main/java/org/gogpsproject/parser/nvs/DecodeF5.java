e5bf4a77cbde57aff1892cbd999d4a2c79a1d5f4
nro2dai <nro2dai@localhost>
2014-03-31 00:19:25 +0900

    modified gmtTS to give a correct time.

--- src/main/java/org/gogpsproject/parser/nvs/DecodeF5.java
+++ src/main/java/org/gogpsproject/parser/nvs/DecodeF5.java
@@ -96,7 +96,7 @@ public class DecodeF5 {
 		int indice;
 		boolean[] temp1;
 		
-		//System.out.println("Num of Satellite: "+ nsv);	
+//		System.out.println("Num of Satellite: "+ nsv);	
 		
 		/*  TOW_UTC, 8 bytes  */
 		bytes = new byte[8];
@@ -112,7 +112,7 @@ public class DecodeF5 {
 		/*  GPS Time Shift, 8 bytes  */
 		bytes = new byte[8];
 		in.read(bytes, 0, bytes.length);
-		double gpsTimeShift = Bits.byteToIEEE754Double(bytes);
+		long gpsTimeShift = (long)Bits.byteToIEEE754Double(bytes);
 		
 		/*  GLONASS Time Shift, 8 bytes  */
 		bytes = new byte[8];
@@ -124,11 +124,14 @@ public class DecodeF5 {
 		//in.read(bytes, 0, bytes.length);	
 		int timeCorrection = in.read();
 		
+//		tow = (tow + gpsTimeShift) / 1000 ;
+		weekN = weekN + 1024 ;
+		
 		long gmtTS = getGMTTS(tow, weekN);
 		Observations o = new Observations(new Time(gmtTS),0);
 		
 //		System.out.println("+----------------  Start of F5  ------------------+");
-//
+
 //		System.out.println("TOW_UTC: "+ utc);			        
 //		System.out.println("Week No.: " + weekN);
 //		System.out.println("GPS-UTC TimeShift: "+ gpsTimeShift);		
@@ -144,8 +147,8 @@ public class DecodeF5 {
 			if(logos!=null) logos.write(data[i]);			
 		}
 		
-		
-		for (int k = 0; k < nsv ; k++) {
+		int gpsCounter = 0;
+		for (int k = 0; k <= nsv ; k++) {
 		
 				ObservationSet os = new ObservationSet();			
 				int offset = k * 30;
@@ -160,113 +163,126 @@ public class DecodeF5 {
 					}
 				int signalType = (int)Bits.bitsToUInt(bits);
 								
-				/* Satellite Number, 1 byte */
-				bits = new boolean[8]; // INT8U
-				indice = 0;
-				temp1 = Bits.intToBits(data[offset + 1], 8);
-					for (int i = 0; i < 8; i++) {
-						bits[indice] = temp1[i];
-						indice++;
-					}
-				int satID = (int)Bits.bitsToUInt(bits);
-				os.setSatID(satID);
+				if (signalType == 2){  // 1:GLONASS, 2: GPS, 4: SBAS, 8:Galileo
 				
-				/* A carrier Number for GLONASS, 1 bytes */
-				bits = new boolean[8]; // INT8U
-				indice = 0;
-				temp1 = Bits.intToBits(data[offset + 1 + 1 ], 8);
-					for (int i = 0; i < 8; i++) {
-						bits[indice] = temp1[i];
-						indice++;
-					}
-				int carrierNum = (int)Bits.bitsToUInt(bits);
-					
-				/* SNR (dB-Hz), 1 byte */
-				bits = new boolean[8]; // INT8U
-				indice = 0;
-				temp1 = Bits.intToBits(data[offset + 1 + 1 + 1], 8);
-					for (int i = 0; i < 8; i++) {
-						bits[indice] = temp1[i];
-						indice++;
-					}
-				int snr = (int)Bits.bitsToUInt(bits);
-				os.setSignalStrength(ObservationSet.L1, snr);
-				
-				/*  Carrier Phase (cycles), 8 bytes  */		
-				bits = new boolean[8 * 8]; // FP64
-				indice = 0;
-				for (int j = offset + 1 + 1 + 1 + 8 ; j >=  1 + 1 + 1 + 1 + offset; j--) {
-					temp1 = Bits.intToBits(data[j], 8);
-					for (int i = 0; i < 8; i++) {
-						bits[indice] = temp1[i];
-						indice++;
-					}
-				}
-				double carrierPhase = UnsignedOperation.toDouble(Bits.tobytes(bits));
-				os.setPhase(ObservationSet.L1, carrierPhase);
-				
-				/* C/A Pseudo Range (ms), 8 bytes  */
-				bits = new boolean[8 * 8]; // FP64
-				indice = 0;
-				for (int j = offset + 1 + 1 + 1 + 8 + 8 ; j >=  1 + 1 + 1 + 1 + 8 + offset; j--) {
-					temp1 = Bits.intToBits(data[j], 8);
-					for (int i = 0; i < 8; i++) {
-						bits[indice] = temp1[i];
-						indice++;
-					}
-				}
-				double pseudoRange = UnsignedOperation.toDouble(Bits.tobytes(bits));
-				os.setCodeC(ObservationSet.L1, pseudoRange);
+							/* Satellite Number, 1 byte */
+							bits = new boolean[8]; // INT8U
+							indice = 0;
+							temp1 = Bits.intToBits(data[offset + 1], 8);
+								for (int i = 0; i < 8; i++) {
+									bits[indice] = temp1[i];
+									indice++;
+								}
+							int satID = (int)Bits.bitsToUInt(bits);
+							os.setSatID(satID);
+							
+							if (satID != 1 && satID != 33 ){  // satID:33 is QZSS and 1 is ??
+//							if (satID != 1  ){
+							
+										/* A carrier Number for GLONASS, 1 bytes */
+										bits = new boolean[8]; // INT8U
+										indice = 0;
+										temp1 = Bits.intToBits(data[offset + 1 + 1 ], 8);
+											for (int i = 0; i < 8; i++) {
+												bits[indice] = temp1[i];
+												indice++;
+											}
+										int carrierNum = (int)Bits.bitsToUInt(bits);
+											
+										/* SNR (dB-Hz), 1 byte */
+										bits = new boolean[8]; // INT8U
+										indice = 0;
+										temp1 = Bits.intToBits(data[offset + 1 + 1 + 1], 8);
+											for (int i = 0; i < 8; i++) {
+												bits[indice] = temp1[i];
+												indice++;
+											}
+										int snr = (int)Bits.bitsToUInt(bits);
+										os.setSignalStrength(ObservationSet.L1, snr);
+										
+										/*  Carrier Phase (cycles), 8 bytes  */		
+										bits = new boolean[8 * 8]; // FP64
+										indice = 0;
+										for (int j = offset + 1 + 1 + 1 + 8 ; j >=  1 + 1 + 1 + 1 + offset; j--) {
+											temp1 = Bits.intToBits(data[j], 8);
+											for (int i = 0; i < 8; i++) {
+												bits[indice] = temp1[i];
+												indice++;
+											}
+										}
+										double carrierPhase = UnsignedOperation.toDouble(Bits.tobytes(bits));
+										os.setPhase(ObservationSet.L1, carrierPhase);
+										
+										/* C/A Pseudo Range (ms), 8 bytes  */
+										bits = new boolean[8 * 8]; // FP64
+										indice = 0;
+										for (int j = offset + 1 + 1 + 1 + 8 + 8 ; j >=  1 + 1 + 1 + 1 + 8 + offset; j--) {
+											temp1 = Bits.intToBits(data[j], 8);
+											for (int i = 0; i < 8; i++) {
+												bits[indice] = temp1[i];
+												indice++;
+											}
+										}
+										double pseudoRange = UnsignedOperation.toDouble(Bits.tobytes(bits));
+										os.setCodeC(ObservationSet.L1, pseudoRange);
+						
+										/*  Doppler Frequency(Hz), 8 bytes  */
+										bits = new boolean[8 * 8]; // FP64
+										indice = 0;
+										for (int j = offset + 1 + 1 + 1 + 8 + 8 + 8 ; j >=  1 + 1 + 1 + 1 + 8 + 8 + offset; j--) {
+											temp1 = Bits.intToBits(data[j], 8);
+											for (int i = 0; i < 8; i++) {
+												bits[indice] = temp1[i];
+												indice++;
+											}
+										}
+										double dopperFrequency = UnsignedOperation.toDouble(Bits.tobytes(bits));
+										float d1 = (float)dopperFrequency; 
+										os.setDoppler(ObservationSet.L1, d1);
+						
+										/* Raw Data Flags, 1 byte */
+										bits = new boolean[8]; // INT8U
+										indice = 0;
+										temp1 = Bits.intToBits(data[offset + 1 + 1 + 1 + 1 + 8 + 8 + 8], 8);
+											for (int i = 0; i < 8; i++) {
+												bits[indice] = temp1[i];
+												indice++;
+											}
+										int rawDataFlags = (int)Bits.bitsToUInt(bits);
+								
+										o.setGps(gpsCounter, os);
+									
+										gpsCounter ++ ;
+										
+//										System.out.println("##### Satellite:  "+ k );
+//										System.out.println("Signal_Type: "+ signalType);
+//										System.out.println("Satellite Number: "+ satID);
+//										System.out.println("Carrier Number: "+ carrierNum);
+//										System.out.println("SNR: "+ snr);
+//										System.out.println("Carrier Phase: "+ carrierPhase);	
+//										System.out.println("Pseudo Range: "+ pseudoRange);
+//										System.out.println("Doppler Frequency: "+ d1);
+//										System.out.println("Raw Data Flags: "+ rawDataFlags);
+//										System.out.println("			");
+							}
+							
 
-				/*  Doppler Frequency(Hz), 8 bytes  */
-				bits = new boolean[8 * 8]; // FP64
-				indice = 0;
-				for (int j = offset + 1 + 1 + 1 + 8 + 8 + 8 ; j >=  1 + 1 + 1 + 1 + 8 + 8 + offset; j--) {
-					temp1 = Bits.intToBits(data[j], 8);
-					for (int i = 0; i < 8; i++) {
-						bits[indice] = temp1[i];
-						indice++;
 					}
-				}
-				double dopperFrequency = UnsignedOperation.toDouble(Bits.tobytes(bits));
-				float d1 = (float)dopperFrequency; 
-				os.setDoppler(ObservationSet.L1, d1);
-
-				/* Raw Data Flags, 1 byte */
-				bits = new boolean[8]; // INT8U
-				indice = 0;
-				temp1 = Bits.intToBits(data[offset + 1 + 1 + 1 + 1 + 8 + 8 + 8], 8);
-					for (int i = 0; i < 8; i++) {
-						bits[indice] = temp1[i];
-						indice++;
-					}
-				int rawDataFlags = (int)Bits.bitsToUInt(bits);
-		
-				o.setGps(k, os);
+//					System.out.println("+-----------------  End of F5  -------------------+");
 			
-//				System.out.println("##### Satellite:  "+ k );
-//				System.out.println("Signal_Type: "+ signalType);
-//				System.out.println("Satellite Number: "+ satID);
-//				System.out.println("Carrier Number: "+ carrierNum);
-//				System.out.println("SNR: "+ snr);
-//				System.out.println("Carrier Phase: "+ carrierPhase);	
-//				System.out.println("Pseudo Range: "+ pseudoRange);
-//				System.out.println("Doppler Frequency: "+ d1);
-//				System.out.println("Raw Data Flags: "+ rawDataFlags);
-//				System.out.println("			");
 
-		}
-//		System.out.println("+-----------------  End of F5  -------------------+");
-
-
+			}
+				
 		return o;
+
 		
 		
 	}
 
 	private long getGMTTS(long tow, long week) {
 		Calendar c = Calendar.getInstance();
-		c.setTimeZone(c.getTimeZone());
+		c.setTimeZone(TimeZone.getTimeZone("GMT Time"));
+//		c.setTimeZone(c.getTimeZone());
 		c.set(Calendar.YEAR, 1980);
 		c.set(Calendar.MONTH, Calendar.JANUARY);
 		c.set(Calendar.DAY_OF_MONTH, 6);
